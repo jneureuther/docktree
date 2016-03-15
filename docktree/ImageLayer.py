a598f514b28df2e93e8efecc4af4cb204d6ff325
julian <dev@jneureuther.de>
2016-03-15 17:34:01 +0100

    initial commit
    Signed-off-by: julian <dev@jneureuther.de>

---
+++ docktree/ImageLayer.py
@@ -0,0 +1,160 @@
+class ImageLayer(object):
+    def __init__(self, children=None, parent=None, identifier='', parent_identifier='', tags=None):
+        """
+        create and initialize a new ImageLayer object
+        :param children: list of children as ImageLayer objects
+        :param parent: parent as ImageLayer object
+        :param identifier: unique string
+        :param parent_identifier: unique identifier of parent layer
+        :param tags: list of tags in unicode
+        """
+
+        self._children = children if children is not None else []
+        self._parent = parent
+        self._identifier = identifier
+        self._parent_identifier = parent_identifier
+        self._tags = tags if tags is not None else []
+
+    def __repr__(self):
+        """
+        :return: string containing a printable representation of a ImageLayer object
+        """
+        return self._identifier[:12] + ' Tags: ' + str(self._tags)
+
+    def print_children(self, indentation=''):
+        """
+        print a tree following the double-linked list
+        :param indentation: indentation for the current layer
+        """
+        if self._parent_identifier == '':
+            print('-', self)
+        elif len(self.parent.children) >= 1:
+            print(indentation, '|-', self)
+        for child in self._children:
+            child.print_children(indentation + "  ")
+
+    @property
+    def children(self):
+        """
+        :return: list of all children
+        :rtype: list
+        """
+        return self._children
+
+    @children.setter
+    def children(self, children):
+        """
+        add a list of children
+        :param children: children to append
+        """
+        self._children = children
+
+    @children.deleter
+    def children(self):
+        """
+        delete all children
+        """
+        del self._children
+
+    def remove_child(self, child):
+        """
+        remove a specific child from the list of children
+        :param child: child to remove
+        """
+        self._children.remove(child)
+
+    @property
+    def parent(self):
+        """
+        get the parent object
+        :return: parent object
+        :rtype: object
+        """
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent):
+        """
+        set the parent
+        :param parent: parent object
+        """
+        self._parent = parent
+
+    @parent.deleter
+    def parent(self):
+        """
+        delete the parent
+        """
+        del self._parent
+
+    @property
+    def identifier(self):
+        """
+        get the identifier
+        :return: identifier of the layer
+        :rtype: str
+        """
+        return self._identifier
+
+    @identifier.setter
+    def identifier(self, identifier):
+        """
+        set the identifier
+        :param identifier: identifier to set
+        """
+        self._identifier = identifier
+
+    @identifier.deleter
+    def identifier(self):
+        """
+        delete the identifier
+        """
+        del self._identifier
+
+    @property
+    def parent_identifier(self):
+        """
+        get the identifier of the parent layer
+        :return: identifier of the parent layer
+        :rtype: str
+        """
+        return self._parent_identifier
+
+    @parent_identifier.setter
+    def parent_identifier(self, parent_identifier):
+        """
+        set the identifier of the parent layer
+        :param parent_identifier: identifier of the parent layer
+        """
+        self._parent_identifier = parent_identifier
+
+    @parent_identifier.deleter
+    def parent_identifier(self):
+        """
+        delete the identifier of the parent layer
+        """
+        del self._parent_identifier
+
+    @property
+    def tags(self):
+        """
+        return a list of all tags
+        :return: list of tags applied to the image
+        :rtype: list
+        """
+        return self._tags
+
+    @tags.setter
+    def tags(self, tag):
+        """
+        append a tag to the list of all tags
+        :param tag: tag to append
+        """
+        self._tags.append(tag)
+
+    @tags.deleter
+    def tags(self):
+        """
+        delete all tags
+        """
+        del self._tags
---
+++ docktree/docktree.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+"""
+Analyse dependencies of docker images.
+"""
+
+from __future__ import print_function
+from docktree.ImageLayer import *
+
+import docker
+import sys
+import argparse
+try:
+    import argcomplete
+except ImportError:
+    print("argcomplete not available")
+    pass
+
+
+def analyse_layers():
+    """
+    analyse all layers and compute a tree
+    :return: dict of images
+    :rtype: dict
+    """
+    docker_cli = docker.Client(base_url='unix://var/run/docker.sock')
+    images = docker_cli.images(all=True)
+
+    layers = {}
+
+    for image in images:
+        layer = ImageLayer(
+            identifier=image['Id'],
+            parent_identifier=image['ParentId'],
+            tags=image['RepoTags']
+        )
+        layers[image['Id']] = layer
+
+    for layer_id, layer in layers.items():
+        if layer.parent_identifier != '':
+            layers[layer.parent_identifier].children.append(layer)
+            layer.parent = layers[layer.parent_identifier]
+
+    return layers
+
+
+def print_tree(heads):
+    """
+    print a tree starting at heads to stdout
+    :param heads: heads of the tree
+    """
+    for head in heads:
+        head.print_children()
+
+
+def get_heads(layers):
+    """
+    return heads of a given tree
+    :param layers: double linked list
+    :return: heads of the tree
+    :rtype: list
+    """
+    heads = []
+    for layer_id, layer in layers.items():
+        if layer.parent_identifier == '':
+            heads.append(layer)
+    return heads
+
+
+def remove_untagged_layers(layers):
+    """
+    remove all untagged layers from the tree
+    :param layers: dict containing all layers
+    :return: tree without untagged layers
+    :rtype: dict
+    """
+    empty_tags = []
+
+    for layer_id, layer in layers.items():
+        if layer.tags[0] == '<none>:<none>':
+            empty_tags.append(layer_id)
+            if layer.parent is not None:
+                layer.parent.remove_child(layer)
+                for child in layer.children:
+                    layer.parent.children.append(child)
+            else:
+                for child in layer.children:
+                    child.parent_identifier = ''
+                    child.parent = None
+            if layer.children is not None:
+                for child in layer.children:
+                    child.parent = layer.parent
+                    child.parent_identifier = layer.parent_identifier
+
+    for tag in empty_tags:
+        layers.pop(tag)
+
+    return layers
+
+
+def parse_args(argv=sys.argv[1:]):
+    """
+    :param argv: arguments which we get called with
+    :return: the parsed arguments as object (see argparse doc)
+    :rtype: object
+    """
+
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        '-i',
+        '--intermediate',
+        action='store_true',
+        dest='print_intermediate',
+        default=False,
+        help='print intermediate (untagged) layers'
+    )
+
+    if 'argcomplete' in globals().keys():
+        argcomplete.autocomplete(parser)
+
+    return parser.parse_args(argv)
+
+
+def main():
+    """
+    parse arguments and run the desired action
+    """
+    args = parse_args()
+
+    layers = analyse_layers()
+    if not args.print_intermediate:
+        layers = remove_untagged_layers(layers)
+    heads = get_heads(layers)
+    print_tree(heads)
+
+if __name__ == '__main__':
+    main()
