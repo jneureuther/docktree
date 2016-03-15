aa4687e7d525dbfffbb2dccfc80f87befd3d0a55
julian <dev@jneureuther.de>
2016-03-15 17:36:08 +0100

    add code for vagrant
    Signed-off-by: julian <dev@jneureuther.de>

---
+++ vagrant-provisioning.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+sudo su
+
+echo -e "\n==== updating all packages ===="
+dnf -y upgrade
+
+echo -e "\n==== installing dependencies ===="
+dnf -y install docker vim python3-ipython
+
+echo -e "\n==== start docker and add user vagrant to docker group"
+systemctl start docker
+getent group docker || groupadd docker
+gpasswd -a vagrant docker
+systemctl restart docker
+
+echo -e "\n==== generate test data ===="
+docker pull busybox
+docker run --name foo busybox touch foo
+docker commit foo foo
+docker rm foo
+docker run --name bar busybox touch bar
+docker commit bar bar
+docker rm bar
+docker run --name baz bar touch baz
+docker commit baz baz
+docker rm baz
+
+echo -e "\n==== install docktree ===="
+cd /vagrant && ./setup.py develop && cd -
+activate-global-python-argcomplete
