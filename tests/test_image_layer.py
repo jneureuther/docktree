b9c6a6b3d99aeba635ea4053e2041abb31adffe8
sedrubal <dev@sedrubal.de>
2016-03-22 05:02:56 +0100

    Move generate helper functions outside testclass

--- tests/test_image_layer.py
+++ tests/test_image_layer.py
@@ -14,26 +14,26 @@ from docktree.ImageLayer import ImageLayer
 from docktree.ImageLayer import _convert_size
 
 
+def _generate_valid_identifier():
+    """:return a random but valid identifier for image layers"""
+    allowed_id_chars = list(set(string.hexdigits.lower()))
+    return ''.join(
+        (random.choice(allowed_id_chars) for i in range(64))
+    )
+
+
+def _generate_tag():
+    """:return a random but valid tag for image layers"""
+    allowed_tag_chars = string.ascii_letters + string.digits
+    return ''.join(
+        (random.choice(allowed_tag_chars) for i in range(
+            random.randint(1, 100)
+        ))
+    )
+
+
 class TestImageLayer(unittest.TestCase):
     """Test the ImageLayer abstraction class"""
-
-    @staticmethod
-    def _generate_valid_identifier():
-        """:return a random but valid identifier for image layers"""
-        allowed_id_chars = list(set(string.hexdigits.lower()))
-        return ''.join(
-            (random.choice(allowed_id_chars) for i in range(64))
-        )
-
-    @staticmethod
-    def _generate_tag():
-        """:return a random but valid tag for image layers"""
-        allowed_tag_chars = string.ascii_letters + string.digits
-        return ''.join(
-            (random.choice(allowed_tag_chars) for i in range(
-                random.randint(1, 100)
-            ))
-        )
 
     def test_init_without_id(self):
         """test if identifier is required"""
@@ -41,31 +41,29 @@ class TestImageLayer(unittest.TestCase):
 
     def test_identifier_prop(self):
         """test the identifier property"""
-        identifier = TestImageLayer._generate_valid_identifier()
+        identifier = _generate_valid_identifier()
         layer = ImageLayer(identifier)
         self.assertEqual(identifier, layer.identifier)
         # change identifier
         self.assertRaises(
             AttributeError, setattr,
-            layer, 'identifier', TestImageLayer._generate_valid_identifier()
+            layer, 'identifier', _generate_valid_identifier()
         )
 
     def test_tags_prop(self):
         """test the tags property"""
-        identifier = TestImageLayer._generate_valid_identifier()
-        tags = [TestImageLayer._generate_tag() for i in range(
-            random.randint(0, 50)
-        )]
+        identifier = _generate_valid_identifier()
+        tags = [_generate_tag() for i in range(random.randint(0, 50))]
         layer = ImageLayer(identifier, tags=tags)
         self.assertEqual(layer.tags, tags)
-        newtag = TestImageLayer._generate_tag()
+        newtag = _generate_tag()
         tags += newtag
         layer.tags.append(tags)
         self.assertEqual(layer.tags, tags)
 
     def test_size_prop(self):
         """test the tags property"""
-        identifier = TestImageLayer._generate_valid_identifier()
+        identifier = _generate_valid_identifier()
         size = random.randint(0, 9999999999999)
         layer = ImageLayer(identifier, size=size)
         self.assertEqual(layer.size, size)
@@ -77,14 +75,14 @@ class TestImageLayer(unittest.TestCase):
 
     def test_parent_children_prop(self):
         """test the parent and child property"""
-        id_child = TestImageLayer._generate_valid_identifier()
+        id_child = _generate_valid_identifier()
         layer_child = ImageLayer(
             identifier=id_child,
             tags=['child'],
             parent=None,
             children=None,
         )
-        id_parent = TestImageLayer._generate_valid_identifier()
+        id_parent = _generate_valid_identifier()
         layer_parent = ImageLayer(
             identifier=id_parent,
             tags=['parent'],
@@ -101,8 +99,8 @@ class TestImageLayer(unittest.TestCase):
 
     def test_repr(self):
         """test the __repr__ function"""
-        identifier = TestImageLayer._generate_valid_identifier()
-        tags = [TestImageLayer._generate_tag()]
+        identifier = _generate_valid_identifier()
+        tags = [_generate_tag()]
         size = random.randint(0, 9999999999999)
         size_human = _convert_size(size)
         layer = ImageLayer(identifier, tags=tags, size=size)
@@ -127,7 +125,7 @@ class TestImageLayer(unittest.TestCase):
 
     def test_print_tree(self):
         """test the print_tree function"""
-        id_child = TestImageLayer._generate_valid_identifier()
+        id_child = _generate_valid_identifier()
         size_child = 42*1024*1024*1024
         layer_child = ImageLayer(
             identifier=id_child,
@@ -136,7 +134,7 @@ class TestImageLayer(unittest.TestCase):
             children=None,
             size=size_child,
         )
-        id_parent = TestImageLayer._generate_valid_identifier()
+        id_parent = _generate_valid_identifier()
         size_parent = 10*1024*1024
         layer_parent = ImageLayer(
             identifier=id_parent,
