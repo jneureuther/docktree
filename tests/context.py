a7df34440d8649120fc86befe2674a40440585b0
sedrubal <dev@sedrubal.de>
2016-03-22 04:02:12 +0100

    Remove context.py (frickel frickel)

--- tests/context.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath('..'))
-
-from bin import docktree_cli
-from docktree import docktree
-from docktree.ImageLayer import ImageLayer
-from docktree.ImageLayer import _convert_size
