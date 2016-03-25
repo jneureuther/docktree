ce8f331d591d0f3363c7980c3f4e022d9f85eaa8
sedrubal <dev@sedrubal.de>
2016-03-25 02:48:12 +0100

    Shrink bad imports ;), add .landscape.yml

--- docktree/__init__.py
+++ docktree/__init__.py
@@ -1,3 +1,1 @@
-from .docktree import analyse_layers
-from .docktree import get_heads
-from .docktree import remove_untagged_layers
+from .docktree import analyse_layers, get_heads, remove_untagged_layers
