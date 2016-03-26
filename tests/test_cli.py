041c3865894eef1d872b8b7f91aabe1f3bbf2777
sedrubal <dev@sedrubal.de>
2016-03-26 01:26:38 +0100

    Add unittest for cli.print_tree

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,7 @@ import sys
 import os
 import random
 import json
+import re
 
 sys.path.insert(0, os.path.abspath('.'))
 
@@ -20,6 +21,7 @@ def generate_random_layer():
     return ImageLayer(
         identifier=generate_valid_identifier(),
         tags=[generate_tag() for _ in range(random.randint(0, 2))],
+        size=random.randint(0, 1024*1024*1024)
     )
 
 
@@ -37,3 +39,77 @@ class TestCli(unittest.TestCase):
                     parent=self.layers[j],
                     child=self.layers[i],
                 )
+        self.heads = [layer for layer in self.layers if layer.parent is None]
+
+    def test_print_tree_invalid(self):
+        """test the print_tree function with an invalid output_format"""
+        self.assertRaises(
+            ValueError, cli.print_tree, self.heads, output_format='foobar')
+
+    def test_print_tree_json(self):
+        """test the print_tree function with json as output_format"""
+        text = cli.print_tree(self.heads, output_format='json')
+        json_heads = json.loads(text)
+        self.assertEqual(len(json_heads), len(self.heads))
+        for i in range(len(self.heads)):
+            self.assertEqual(json_heads[i], dict(self.heads[i]))
+
+    def test_print_tree_default(self):
+        """test if the default output_format of print_tree is ascii"""
+        text = cli.print_tree(self.heads, output_format='ascii')
+        text_default = cli.print_tree(self.heads)
+        self.assertEqual(text, text_default)
+
+    def test_print_tree_ascii(self):
+        """test the print_tree function with ascii as output_format"""
+        text = cli.print_tree(self.heads, output_format='ascii')
+        line_regex = re.compile(
+            r"(?P<indent> {0,}\|?- )"
+            r"(?P<id>[0-9a-f]{12})"
+            r" Tags: (?P<tags>\[(\'[a-zA-Z0-9:\./]{1,}\'(, )?){0,}\])"
+            r" Size: (?P<size>\d{1,}(\.\d)? ([KMGT]i)?B)"
+        )
+        text_heads = []
+        for line in text.splitlines():
+            matches = line_regex.match(line)
+            self.assertIsNotNone(matches)
+            indent = matches.group('indent')
+            identifier = matches.group('id')
+            size_str = matches.group('size')
+            size = float(size_str.split(' ')[0])
+            convert = {'B': 0, 'KiB': 1, 'MiB': 2, 'GiB': 3, 'TiB': 4}
+            size *= 1024**convert[size_str.split(' ')[1]]
+            tags = json.loads(matches.group('tags').replace("'", '"'))
+            self.assertIsInstance(tags, list)
+            layer = ImageLayer(
+                identifier=identifier,
+                tags=tags,
+                size=size,
+            )
+            if indent == '- ':
+                text_heads.append(layer)
+            elif indent == '  |- ':
+                ImageLayer.join_parent_child(
+                    parent=text_heads[-1],
+                    child=layer,
+                )
+            # else: we're only testing the heads and their children
+            # (not the childs of childs)
+        for i in range(len(self.heads)):
+            self.assertEqual(
+                text_heads[i].identifier,
+                self.heads[i].identifier[:12]
+            )
+            self.assertEqual(text_heads[i].tags, self.heads[i].tags)
+            self.assertAlmostEqual(text_heads[i].size, self.heads[i].size, -5)
+            self.assertEqual(
+                len(text_heads[i].children),
+                len(self.heads[i].children)
+            )
+            if self.heads[i].parent:
+                self.assertEqual(
+                    text_heads[i].parent.identifier,
+                    self.heads[i].parent.identifier
+                )
+            else:
+                self.assertIsNone(text_heads[i].parent)
