f4bbc52c8125939a8a715c0c407029edf0302cb1
sedrubal <dev@sedrubal.de>
2016-03-30 15:57:25 +0200

    Add unittests for analyze_layers

--- tests/test_docktree.py
+++ tests/test_docktree.py
@@ -16,6 +16,28 @@ from tests.test_image_layer import generate_valid_identifier, generate_tag
 from docktree import docktree
 
 
+def generate_random_api_layer(max_tag_count=2):
+    """
+    :return: a dict containing a layer like it's provided by docker api
+    :param max_tag_count: the maximum count of tags the layer should have
+    """
+    repo_digests = []
+    tags = [generate_tag() for _ in range(max_tag_count)]
+    if not tags:
+        tags.append('<none>:<none>')
+        repo_digests.append('<none>@<none>')
+    return {
+        'Created': random.randint(0, 3000000000),
+        'Id': generate_valid_identifier(),
+        'ParentId': '',
+        'Labels': {},
+        'RepoTags': tags,
+        'RepoDigests': repo_digests,
+        'Size': 0,
+        'VirtualSize': random.randint(0, 1024*1024*1024*1024),
+    }
+
+
 class TestDocktree(unittest.TestCase):
     """Test the docktree.docktree module"""
 
@@ -30,6 +52,44 @@ class TestDocktree(unittest.TestCase):
         # example implementation of get_heads
         self.heads = [lay for lay in self.layers.values() if lay.is_head()]
 
+    def test_analyze_layers(self):
+        """docstring for test_analyze_layers"""
+        api_list = [generate_random_api_layer() for _ in range(15)]
+        # connect parent and child
+        for i, api_layer in enumerate(api_list[:-1]):
+            api_layer['ParentId'] = random.choice(api_list[i+1:])['Id']
+        analyzed_dict = docktree.analyze_layers(api_list)
+        # check for consistency (key is identifier of value)
+        for identifier, layer in analyzed_dict.items():
+            self.assertEqual(layer.identifier, identifier)
+        # check if all layers from api are in the dict
+        self.assertEqual(len(analyzed_dict), len(api_list))
+        for api_layer in api_list:
+            self.assertIn(api_layer['Id'], analyzed_dict.keys())
+            analyzed_layer = analyzed_dict[api_layer['Id']]
+            self.assertEqual(analyzed_layer.size, api_layer['VirtualSize'])
+            if api_layer['ParentId']:
+                self.assertIsNotNone(
+                    analyzed_layer.parent,
+                    "Parent should not be None. There was a ParentId in API"
+                )
+                self.assertEqual(
+                    analyzed_layer.parent.identifier,
+                    api_layer['ParentId']
+                )
+                self.assertIn(
+                    analyzed_layer,
+                    analyzed_layer.parent.children,
+                    "The current layer should be in parent's children list"
+                )
+                for child in analyzed_layer.children:
+                    self.assertEqual(child.parent, analyzed_layer)
+            else:
+                self.assertIsNone(
+                    analyzed_layer.parent,
+                    "Parent should be None. There was no ParentId in API"
+                )
+
     def test_get_heads(self):
         """test the get_heads method returns a list of heads"""
         heads = docktree.get_heads(self.layers)
@@ -43,9 +103,9 @@ class TestDocktree(unittest.TestCase):
         tagged_layers = docktree.remove_untagged_layers(test_layers)
         # check for consistency (key is identifier of value)
         for identifier, layer in test_layers.items():
-            self.assertEqual(test_layers[identifier].identifier, identifier)
+            self.assertEqual(layer.identifier, identifier)
         for identifier, layer in tagged_layers.items():
-            self.assertEqual(tagged_layers[identifier].identifier, identifier)
+            self.assertEqual(layer.identifier, identifier)
         # check if test_layers has changed (when copy() is missing)
         for identifier, layer in self.layers.items():
             self.assertDictEqual(dict(layer), dict(test_layers[identifier]))
