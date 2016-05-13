3e16c63a7ba1467880e722e483ffa98a43121237
julian <dev@jneureuther.de>
2016-05-14 00:46:04 +0200

    add new unit tests for get_heads
    Signed-off-by: julian <dev@jneureuther.de>

--- tests/test_docktree.py
+++ tests/test_docktree.py
@@ -14,6 +14,7 @@ from tests.test_cli import generate_random_layer, connect_layers_random
 from tests.test_image_layer import generate_valid_identifier, generate_tag
 
 from docktree import docktree
+from docktree.ImageLayer import ImageLayer
 
 
 def generate_random_api_layer(max_tag_count=2):
@@ -51,6 +52,44 @@ class TestDocktree(unittest.TestCase):
         connect_layers_random(list(self.layers.values()))
         # example implementation of get_heads
         self.heads = [lay for lay in self.layers.values() if lay.is_head()]
+        self.static_layers = {
+            'ezue4PoF7Im1zae2kie9uokua3ze3dae8ode9edooPhah7Eifaekae' +
+            'F9aith1EeX' :
+            ImageLayer(
+                identifier='ezue4PoF7Im1zae2kie9uokua3ze3dae8ode9ed' +
+                'ooPhah7EifaekaeF9aith1EeX',
+                tags=['foo1/bar:baz'],
+                size=1
+            ),
+            'xielael0biezoreire7ieth9teeKohphoh2Cheew8uoreeYoosa1ja' +
+            'tha2eejeic' :
+            ImageLayer(
+                identifier='xielael0biezoreire7ieth9teeKohphoh2Chee' +
+                'w8uoreeYoosa1jatha2eejeic',
+                tags=['foo2/bar:baz'],
+                size=2
+            ),
+            'zoomee1eiNgieF8ees0Eemieruaneeyee9so5na8ahdo0chaizahth' +
+            'ahph7eekub' :
+            ImageLayer(
+                identifier='zoomee1eiNgieF8ees0Eemieruaneeyee9so5na' +
+                '8ahdo0chaizahthahph7eekub',
+                tags=['foo3/bar:baz'],
+                size=3
+            )
+        }
+        self.static_layers['ezue4PoF7Im1zae2kie9uokua3ze3dae8ode9ed' +
+                           'ooPhah7EifaekaeF9aith1EeX'].parent = \
+        self.static_layers['xielael0biezoreire7ieth9teeKohphoh2Chee' +
+                           'w8uoreeYoosa1jatha2eejeic']
+        self.static_layers['xielael0biezoreire7ieth9teeKohphoh2Chee' +
+                           'w8uoreeYoosa1jatha2eejeic'].parent = \
+        self.static_layers['zoomee1eiNgieF8ees0Eemieruaneeyee9so5na' +
+                           '8ahdo0chaizahthahph7eekub']
+        self.static_heads = [
+            self.static_layers['zoomee1eiNgieF8ees0Eemieruaneeyee9so5na8ahdo0cha' +
+                          'izahthahph7eekub']
+        ]
 
     def test_analyze_layers(self):
         """docstring for test_analyze_layers"""
@@ -90,10 +129,31 @@ class TestDocktree(unittest.TestCase):
                     "Parent should be None. There was no ParentId in API"
                 )
 
-    def test_get_heads(self):
+    def test_get_all_heads(self):
         """test the get_heads method returns a list of heads"""
         heads = docktree.get_heads(self.layers)
         self.assertListEqual(heads, self.heads)
+        for layer in heads:
+            self.assertTrue(layer.is_head())
+
+    def test_get_heads_for_id(self):
+        """test the get_heads function with a given id"""
+        heads = docktree.get_heads(self.static_layers, 'ezue4PoF7Im')
+        self.assertListEqual(heads, self.static_heads)
+        for layer in heads:
+            self.assertTrue(layer.is_head())
+
+    def test_get_heads_for_name(self):
+        """test the get_heads function with a given name"""
+        heads = docktree.get_heads(self.static_layers, 'foo1/bar:baz')
+        self.assertListEqual(heads, self.static_heads)
+        for layer in heads:
+            self.assertTrue(layer.is_head())
+
+    def test_get_heads_for_head(self):
+        """test the get_heads function with a head element"""
+        heads = docktree.get_heads(self.static_layers, 'foo3/bar:baz')
+        self.assertListEqual(heads, self.static_heads)
         for layer in heads:
             self.assertTrue(layer.is_head())
 
