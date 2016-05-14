9878969bab4026cff4b578dcec374a093d938f74
julian <dev@jneureuther.de>
2016-05-14 04:11:34 +0200

    test unicode and ascii in print_tree
    Signed-off-by: julian <dev@jneureuther.de>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -66,14 +66,18 @@ class TestCli(unittest.TestCase):
             self.assertDictEqual(json_heads[i], dict(layer))
 
     def test_print_tree_default(self):
-        """test if the default output_format of print_tree is ascii"""
-        text = cli.print_tree(self.heads, output_format='ascii')
+        """test if the default output_format of print_tree is plain"""
+        text = cli.print_tree(self.heads, output_format='plain')
         text_default = cli.print_tree(self.heads)
         self.assertEqual(text, text_default)
 
-    def test_print_tree_ascii(self):
-        """test the print_tree function with ascii as output_format"""
-        text = cli.print_tree(self.heads, output_format='ascii')
+    def test_print_tree_plain_ascii(self):
+        """test the print_tree function with plain as output_format and ascii"""
+        text = cli.print_tree(
+            self.heads,
+            output_format='plain',
+            encoding='ascii'
+        )
         line_regex = re.compile(
             r"(?P<indent> {0,}\|?- )"
             r"(?P<id>[0-9a-f]{12})"
@@ -100,6 +104,62 @@ class TestCli(unittest.TestCase):
             if indent == '- ':
                 text_heads.append(layer)
             elif indent == '  |- ':
+                ImageLayer.join_parent_child(
+                    parent=text_heads[-1],
+                    child=layer,
+                )
+            # else: we're only testing the heads and their children
+            # (not the childs of childs)
+        for i, layer in enumerate(self.heads):
+            self.assertEqual(
+                text_heads[i].identifier,
+                layer.identifier[:12]
+            )
+            self.assertListEqual(text_heads[i].tags, layer.tags)
+            self.assertEqual(
+                _convert_size(text_heads[i].size),
+                _convert_size(layer.size)
+            )
+            self.assertEqual(
+                len(text_heads[i].children),
+                len(layer.children)
+            )
+            self.assertTrue(text_heads[i].is_head())
+
+    def test_print_tree_plain_utf8(self):
+        """test the print_tree function with plain as output_format and utf-8"""
+        text = cli.print_tree(
+            self.heads,
+            output_format='plain',
+            encoding='utf-8'
+        )
+        line_regex = re.compile(
+            r"(?P<indent> {0,}\u2514?\u2500 )"
+            r"(?P<id>[0-9a-f]{12})"
+            r" Tags: (?P<tags>\[(\'[a-zA-Z0-9:\./]{1,}\'(, )?){0,}\])"
+            r" Size: (?P<size>\d{1,}(\.\d)? ([KMGT]i)?B)",
+            re.UNICODE
+        )
+        text_heads = []
+        for line in text.splitlines():
+            matches = line_regex.match(line)
+            self.assertIsNotNone(matches)
+            indent = matches.group('indent')
+            identifier = matches.group('id')
+            size_str = matches.group('size')
+            size = float(size_str.split(' ')[0])
+            convert = {'B': 0, 'KiB': 1, 'MiB': 2, 'GiB': 3, 'TiB': 4}
+            size *= 1024**convert[size_str.split(' ')[1]]
+            tags = json.loads(matches.group('tags').replace("'", '"'))
+            self.assertIsInstance(tags, list)
+            layer = ImageLayer(
+                identifier=identifier,
+                tags=tags,
+                size=size,
+            )
+            if indent == '─ ':
+                text_heads.append(layer)
+            elif indent == '  └─ ':
                 ImageLayer.join_parent_child(
                     parent=text_heads[-1],
                     child=layer,
